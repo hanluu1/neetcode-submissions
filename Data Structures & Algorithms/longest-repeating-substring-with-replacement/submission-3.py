32b216b013baf0703239a06aa83754aa3bb76fec
Han Luu <154330914+hanluu1@users.noreply.github.com>
2026-05-26 02:04:25 -0700

    Add: longest-repeating-substring-with-replacement - submission-3

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-3.py
@@ -0,0 +1,24 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        #only replace at most k letters, and return the longest substring
+        #use sliding window 
+        # map to access the char and keep track better
+        #track how many chars match c 
+        #how many replacements needed
+
+        ans = 0
+        left = 0
+        count = {}
+
+        for right in range(len(s)):
+            count[s[right]] = 1 + count.get(s[right], 0)
+
+            while (right - left + 1) - max(count.values()) > k:
+                count[s[left]] -= 1
+                left += 1
+            ans = max(ans, right - left + 1)
+        return ans
+
+           
+
+       
