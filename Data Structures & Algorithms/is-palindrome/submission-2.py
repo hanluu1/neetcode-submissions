1e8bfa22b4c229b50e9c32a8fc276455c9134727
Han Luu <154330914+hanluu1@users.noreply.github.com>
2026-06-11 23:52:12 -0700

    Add: is-palindrome - submission-2

---
+++ Data Structures & Algorithms/is-palindrome/submission-2.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        #two pointer to see if the first char match the last char and continue moving the pointer 
+        l = 0
+        r = len(s) - 1
+        while l < r:
+            #check if the char is alphanumeric or not if not skip that 
+            if not s[l].isalnum():
+                l += 1
+            elif not s[r].isalnum():
+                r -=1
+            else:
+                if s[l].lower() != s[r].lower:
+                    return False    
+                l+=1
+                r+=1
+            return True
+       
+
+                
+            
