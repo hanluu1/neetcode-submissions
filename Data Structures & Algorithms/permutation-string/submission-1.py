3c224d0c6af86a20116e43ca1e919508aa83f8bf
Han Luu <154330914+hanluu1@users.noreply.github.com>
2026-07-15 03:18:33 -0700

    Add: permutation-string - submission-1

---
+++ Data Structures & Algorithms/permutation-string/submission-1.py
@@ -0,0 +1,36 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        #sliding window
+        if len(s1) > len(s2):
+            return False
+        countS1 = [0] * 26
+        countS2 = [0] * 26
+
+        for i in range(len(s1)):
+            countS1[ord(s1[i]) - ord('a')] += 1
+            countS2[ord(s2[i]) - ord('a')] += 1
+            
+        totalMatches = 0
+        for i in range(26):
+            totalMatches += (1 if countS1[i] == countS2[i] else 0)
+
+        l = 0
+        for r in range(len(s1), len(s2)):
+            if totalMatches == 26:
+                return True
+            
+            index = ord(s2[r]) - ord('a')
+            countS2[index] += 1
+            if countS1[index] == countS2[index]:
+                totalMatches += 1
+            elif countS1[index] + 1 == countS2[index]:
+                totalMatches -= 1
+
+            index = ord(s2[l]) - ord('a')
+            countS2[index] -= 1
+            if countS1[index] == countS2[index]:
+                totalMatches += 1
+            elif countS1[index] - 1 == countS2[index]:
+                totalMatches -= 1
+            l+=1
+        return totalMatches == 26
